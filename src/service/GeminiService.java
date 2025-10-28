35687b73acf461c68a87460b7f429d6a11952908
kimjava911 <aibackend4th@gmail.com>
2025-10-28 12:23:40 +0900

    Step02-3

--- src/service/GeminiService.java
+++ src/service/GeminiService.java
@@ -10,16 +10,18 @@ public class GeminiService {
     // 캡슐화 -> GeminiService / Application
     private final HttpClient client = HttpClient.newHttpClient();
     private final String API_KEY = System.getenv("GEMINI_API_KEY");
-    private final String url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent";
+//    private final String url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent";
+    private final String url = "https://generativelanguage.googleapis.com/v1beta/models/%s:generateContent"; // 포맷팅으로 변경시켜줄 것
 
-    public String chat(String input) {
+    private String chatUseModel(String input, String model) {
         // 1. request
         String payload = """
                     {"contents": [{"parts": [{"text": "%s"}]}]}
                     """.formatted(input); // JSON 포맷
+        // String model = "gemini-2.5-flash";
         HttpRequest request = HttpRequest
                 .newBuilder()
-                .uri(URI.create(url))
+                .uri(URI.create(url.formatted(model)))
                 .header("x-goog-api-key", API_KEY)
                 .header("Content-Type", "application/json")
                 .POST(HttpRequest.BodyPublishers.ofString(payload))
@@ -30,12 +32,21 @@ public class GeminiService {
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
             String body = response.body();
             output = body.split("\"text\": \"")[1]
-            .split("\"\\s*}")[0];
+                    .split("\"\\s*}")[0];
             // 정규표현식 패턴
         } catch (Exception ex) {
             ex.getStackTrace();
         }
         // output
         return output;
+    }
+
+    public String chat(String input) {
+        return chatUseModel(input, "gemini-2.5-flash");
+    }
+
+    // 오버로딩
+    public String chat(String input, String model) {
+        return chatUseModel(input, model);
     }
 }
